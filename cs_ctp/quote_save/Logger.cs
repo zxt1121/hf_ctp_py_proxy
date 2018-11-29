73bc1485ac8a82ac1392d031a2ce4c9c79a1eee0
dogvane <dogvane@gmail.com>
2018-11-29 22:34:20 +0800

    1.增加行情数据保存功能

---
+++ cs_ctp/quote_save/Logger.cs
@@ -0,0 +1,241 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// 默认的日志记录器
+    /// 项目使用NLog记录日志
+    /// 但不直接使用NLog的接口
+    /// 而是自定义的一条日志接口和实现
+    /// 这样可以方便切换日志组件
+    /// </summary>
+    public static class Logger
+    {
+        /// <summary>
+        /// 默认构造方法
+        /// </summary>
+        static Logger()
+        {
+            var nLogfile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nlog.config");
+            if (File.Exists(nLogfile))
+            {
+                // 按照文件进行初始化
+                LogManager.LoadConfiguration(nLogfile);
+                s_logger = LogManager.GetCurrentClassLogger();
+            }
+            else
+            {
+                var config = new LoggingConfiguration();
+
+                var console = new ConsoleTarget();
+                console.Layout = @"${date:format=HH\:mm\:ss} ${logger} [${level}] ${message}";
+                config.AddTarget("console", console);
+
+                var file = new FileTarget();
+                config.AddTarget("file", file);
+
+                file.Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss.fff} ${logger} [${level}] ${message} ${onexception:inner=${newline}[Exception]${exception:format=ToString}}";
+                file.FileName = "${basedir}/logs/log${date:format=yyyyMMdd}.txt";
+                file.ArchiveDateFormat = "yyyyMMdd";
+                file.ArchiveEvery = FileArchivePeriod.Day;
+                file.BufferSize = 1024 * 1024;
+                file.CreateDirs = true;
+
+                var rule1 = new LoggingRule("*", LogLevel.Info, console);
+                config.LoggingRules.Add(rule1);
+
+                var rule2 = new LoggingRule("*", LogLevel.Info, file);
+                config.LoggingRules.Add(rule2);
+
+                LogManager.Configuration = config;
+
+                s_logger = LogManager.GetLogger("");
+            }
+        }
+
+        static ILogger s_logger;
+
+        /// <summary>
+        /// 输出Debug信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        public static void Debug(string message)
+        {
+            s_logger.Debug(message);
+        }
+
+        /// <summary>
+        /// 输出Debug信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Debug(string format, params object[] param)
+        {
+            s_logger.Debug(format, param);
+        }
+
+        /// <summary>
+        /// 输出Info信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        public static void Info(string message)
+        {
+            s_logger.Info(message);
+        }
+
+        /// <summary>
+        /// 输出Info信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Info(string format, params object[] param)
+        {
+            s_logger.Info(format, param);
+        }
+
+        /// <summary>
+        /// 输出Warn信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        public static void Warn(string message)
+        {
+            s_logger.Warn(message);
+        }
+
+        /// <summary>
+        /// 输出Warn信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Warn(string format, params object[] param)
+        {
+            s_logger.Warn(format, param);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        public static void Error(string message)
+        {
+            s_logger.Error(message);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Error(string format, string param)
+        {
+            s_logger.Error(format, param);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Error(string format, int param)
+        {
+            s_logger.Error(format, param);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="ex">异常</param>
+        /// <param name="message">日志消息</param>
+        public static void Error(Exception ex, string message)
+        {
+            s_logger.Error(ex, message);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="ex">异常</param>
+        /// <param name="message">日志消息</param>
+        public static void Error(string message, Exception ex)
+        {
+            s_logger.Error(ex, message);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="format">日志格式 {0} {1} 的替换方式</param>
+        /// <param name="param">替换参数</param>
+        public static void Error(Exception ex, string format, params object[] param)
+        {
+            s_logger.Error(ex, format, param);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="param2">替换参数</param>
+        public static void Error(string message, string param1, string param2)
+        {
+            s_logger.Error(message, new object[] { param1, param2 });
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="param2">替换参数</param>
+        /// <param name="param3">替换参数</param>
+        public static void Error(string message, string param1, string param2, string param3)
+        {
+            s_logger.Error(message, new object[] { param1, param2, param3 });
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="ex">异常</param>
+        public static void Error(Exception ex, string message, string param1)
+        {
+            s_logger.Error(ex, message, param1);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="ex">异常</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="param2">替换参数</param>
+        public static void Error(Exception ex, string message, string param1, string param2)
+        {
+            s_logger.Error(ex, message, param1, param2);
+        }
+
+        /// <summary>
+        /// 输出Error信息
+        /// </summary>
+        /// <param name="message">日志消息</param>
+        /// <param name="ex">异常</param>
+        /// <param name="param1">替换参数</param>
+        /// <param name="param2">替换参数</param>
+        /// <param name="param3">替换参数</param>
+        public static void Error(Exception ex, string message, string param1, string param2, string param3)
+        {
+            s_logger.Error(ex, message, param1, param2, param3);
+        }
+
+    }
+}
---
+++ cs_ctp/quote_save/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Common;
+using CsvHelper;
+using HaiFeng;
+using ErrorEventArgs = HaiFeng.ErrorEventArgs;
+
+namespace quote_save
+{
+    /// <summary>
+    /// 行情数据收集并保存到本地的csv里
+    /// </summary>
+    class Program
+    {
+        private static string investor = "008105";
+        private static string pwd = "1";
+
+        static void Main(string[] args)
+        {
+            var qs = new QuoteSave(investor, pwd, "i1901", "rb1901", "rb1905", "rb1910", "pta1905");
+            qs.Run();
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape
+                    || key.Key == ConsoleKey.Spacebar
+                    || key.Key == ConsoleKey.Enter)
+                    break;
+                Thread.Sleep(500);
+            }
+
+            qs.Release();
+        }
+    }
+
+
+    public class QuoteSave
+    {
+        CTPQuote _q = null;
+        string _investor, _pwd, _broker = "9999";
+        private string[] _inst;
+
+        public QuoteSave(string investor, string pwd,params string[] instrument)
+        {
+            _investor = investor;
+            _pwd = pwd;
+            _inst = instrument;
+
+            _q = new CTPQuote("ctp_quote");
+
+            _q.OnFrontConnected += _q_OnFrontConnected;
+            _q.OnRspUserLogin += _q_OnRspUserLogin;
+            _q.OnRspUserLogout += _q_OnRspUserLogout;
+            _q.OnRtnTick += _q_OnRtnTick;
+            _q.OnRtnError += _q_OnRtnError;
+            
+        }
+
+        public void Release()
+        {
+            _q.ReqUserLogout();
+
+            foreach (var stream in this.writers)
+            {
+                try
+                {
+                    stream.Flush();
+                    stream.Close();
+                }
+                catch (Exception e)
+                {
+                    Logger.Error("flush fail.", e);
+                }
+            }
+        }
+
+        public void Run()
+        {
+            _q.ReqConnect("tcp://180.168.146.187:10010");
+        }
+
+        private void _q_OnFrontConnected(object sender, EventArgs e)
+        {
+            Logger.Info("connected");
+            _q.ReqUserLogin(_investor, _pwd, _broker);
+        }
+
+        private void _q_OnRspUserLogin(object sender, IntEventArgs e)
+        {
+            if (e.Value == 0)
+            {
+                Logger.Info($"quote 登录成功:{_investor}");
+                _q.ReqSubscribeMarketData(_inst);
+            }
+            else
+            {
+                //_q.OnFrontConnected -= _q_OnFrontConnected;    //解决登录错误后不断重连导致再不断登录的错误
+                Logger.Info($"quote 登录错误：{e.Value}");
+                _q.ReqUserLogout();
+            }
+        }
+
+        private Dictionary<string, CsvWriter> map = new Dictionary<string, CsvWriter>();
+        private List<TextWriter> writers = new List<TextWriter>();
+        /// <summary>
+        /// 根据期货代码，获得保存的文件位置
+        /// </summary>
+        /// <param name="instrumentId"></param>
+        /// <returns></returns>
+        CsvWriter GetStream(string instrumentId)
+        {
+            if (map.TryGetValue(instrumentId, out CsvWriter writer))
+            {
+                return writer;
+            }
+
+            var id = new Regex(@"[a-z|A-Z]*").Match(instrumentId);
+            var time = DateTime.Now;
+            if (time > DateTime.Now.Date.AddHours(17))
+            {
+                // 这里是夜盘数据，文件名应该用下一个交易日的文件名
+                // todo 这里需要获得交易日日历的功能
+                time = time.AddDays(1);
+            }
+            var path = $"{id}\\{instrumentId}\\{time.ToString("yyyyMMdd")}.csv";
+            var fi = new FileInfo(path);
+            if (!fi.Directory.Exists)
+            {
+                fi.Directory.Create();
+            }
+
+            bool isNew = !File.Exists(path);
+
+            var stream = new StreamWriter(path, true, Encoding.Default);
+            writers.Add(stream);
+
+            writer = new CsvWriter(stream);
+            map[instrumentId] = writer;
+            if (isNew)
+            {
+                writer.WriteHeader(typeof(MarketData));
+            }
+            
+            return writer;
+        }
+
+        private void _q_OnRtnTick(object sender, TickEventArgs e)
+        {
+            var tick = e.Tick;
+            var writer = GetStream(tick.InstrumentID);
+            writer.WriteRecord(tick);
+            writer.NextRecord();            
+
+            Logger.Info(e.Tick.InstrumentID);
+        }
+
+        private void _q_OnRspUserLogout(object sender, IntEventArgs e)
+        {
+            Logger.Info($"quote logout: {e.Value}");
+        }
+
+        private void _q_OnRtnError(object sender, ErrorEventArgs e)
+        {
+            Logger.Info(e.ErrorMsg);
+        }
+    }
+}
